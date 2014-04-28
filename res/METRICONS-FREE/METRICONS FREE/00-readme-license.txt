e23d368c31a554ba508ba8b527b118b254fe475e
oettersa <somekindofmonst3r@yahoo.com>
2014-04-28 18:58:32 -0500

    Initial Commit
    Committed the files starting with Version 1.2

---
+++ res/METRICONS-FREE/METRICONS FREE/00-readme-license.txt
@@ -0,0 +1,35 @@
+METRICONS FREE
+Icons for Windows 8 
+Created by Stoian Oliviu
+WWW: http://iconswindows.com/
+Twitter: @oliviustoian
+
+----------
+
+LICENSE:
+You're free to use these icons only for non-commercial purposes.
+
+ATTRIBUTION:
+None required for use by you, your company, your clients. 
+But if you'd like to spread the word by linking to iconswindows.com from your 
+website, blogpost, Twitter or wherever, your kind words are much appreciated.
+
+QUESTIONS, COMMENTS:
+Contact me via iconswindows.com or olivetty@gmail.com
+
+Ownership and Usage
+All ownership and copyright of the licensed icons remain the property of 
+iconwindows.com and Oliviu Stoian.
+
+Disclaimer
+All licensed icons are provided 'as-is' without warranty of any kind, 
+either expressed or implied.
+    
+Restrictions
+Use for commercial purposes, embed the icons in a font, and insert them in your website using @font-face is restricted. 
+The Icons may not be resold, sublicensed, rented, transferred or otherwise 
+made available for use or detached from a product, software application or web 
+page without express written permission from IconsWindows.
+----------
+You can download all 750 with the code ICNDEP25$OFF with $25 discount
+----------
---
+++ src/mmautoaccept/MMAutoAccept.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmautoaccept;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * TODO:    1. System tray icon and functionality. (low priority)
+ * 
+ * changelog:   1.0 basic functionality
+ *              1.1 added global hotkeys
+ *              1.2 added minimize and close animations, disabled start if program isn't running, displays resolution on top bar, can't run multiple instances
+ * 
+ * @version 1.2 April 25, 2014 (added Global HotKeys)
+ * @author Simon Oetter
+ */
+public class MMAutoAccept {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MMAutoAcceptGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MMAutoAcceptGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MMAutoAcceptGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MMAutoAcceptGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                MMAutoAcceptGUI m;
+                try {
+                    m = new MMAutoAcceptGUI();
+                    
+                    m.setVisible(true);
+                    m.setLocationRelativeTo(null);
+                } catch (IOException ex) {
+                    Logger.getLogger(MMAutoAccept.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                
+            }
+        });
+    }  
+}
